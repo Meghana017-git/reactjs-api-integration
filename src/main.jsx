761f680eb3639aa35bfa31ae4923cfdd4a2ce2dc
dhan17 <dhanushulal17@gmail.com>
2025-01-28 22:32:48 +0530

    tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor } from './app/store'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./app/store";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
-  
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    </PersistGate>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </StrictMode>,
-
-)
+  </StrictMode>
+);
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,52 +2,71 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addCart } from "../cart/CartSlice";
+import { useQuery } from "@tanstack/react-query";
+import { FaSpinner } from "react-icons/fa6";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
- 
+
   useEffect(() => { 
     fetchProducts();
   }, []);
 
   const fetchProducts = async () => {
     try {
+      
       const response = await axios.get("https://fakestoreapi.com/products");
       setProducts(response.data);
     } catch (error) {
       console.error("error fetching products", error);
     }
   };
+    const { isLoading } = useQuery({
+      queryKey: ["products"],
+      queryFn: fetchProducts,
+    });
+    
   return (
     <>
-      <div className="container mx-auto">
-        <div className="grid grid-cols-4 gap-8">
-          {products.map((product) => {
-            return (
-              <div
-                key={product.id}
-                className="max-w-sm border border-gray-200 rounded-lg shadow"
-              >
-                <img
-                  className="w-full h-64 object-contain p-4"
-                  src={product.image}
-                ></img>
-                <div className="px-4 py-2">
-                  <h2 className="text-xl font-semibold text-cyan-800">
-                    {product.title}
-                  </h2>
-                  <h5 className="text-xl text-black">₹{product.price}</h5>
-                  <p className="">{product.description}</p>
-                </div>
-                <div className="px-4 py-2">
-                <button onClick={() =>  dispatch(addCart(product))} className="bg-blue-500 text-white px-3 py-2 hover:bg-sky-700">Add to Cart</button>
-                </div>
-              </div>
-            );
-          })}
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <FaSpinner className="animate-spin text-3xl text-blue-500" />
         </div>
-      </div>
+      ) : (
+        <div className="container mx-auto">
+          <div className="grid grid-cols-4 gap-8">
+            {products.map((product) => {
+              return (
+                <div
+                  key={product.id}
+                  className="max-w-sm border border-gray-200 rounded-lg shadow"
+                >
+                  <img
+                    className="w-full h-64 object-contain p-4"
+                    src={product.image}
+                  ></img>
+                  <div className="px-4 py-2">
+                    <h2 className="text-xl font-semibold text-cyan-800">
+                      {product.title}
+                    </h2>
+                    <h5 className="text-xl text-black">₹{product.price}</h5>
+                    <p className="">{product.description}</p>
+                  </div>
+                  <div className="px-4 py-2">
+                    <button
+                      onClick={() => dispatch(addCart(product))}
+                      className="bg-blue-500 text-white px-3 py-2 hover:bg-sky-700"
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </>
   );
 };
