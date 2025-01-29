684be6c8d3295ba89061722562ac858c007216f4
dhan17 <dhanushulal17@gmail.com>
2025-01-29 11:00:43 +0530

    loading,data applied

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,18 @@ import { store } from "./app/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./app/store";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
